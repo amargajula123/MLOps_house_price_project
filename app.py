52ad3c98ea80f9f5b184b30c5813c07d96ba8728
Amar gajula <amargajula123@gmail.com>
2025-02-22 14:57:00 +0530

    added exception and logger

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask
-# import sys
-# from housing.logger import logging
-# from housing.exception import HosingException
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning MODELS And its Intution to get some predictions"
+    #logging.info("we are testing logging module")
+    #return "Starting Machine Learning MODELS And its Intution to get some predictions"
 
-    # try:
-    #     raise Exception("we are testing Custome Exception")
-    # except Exception as e:
-    #     housing=HosingException(e,sys)
-    #     logging.info(housing.error_massage)
+    try:
+        raise Exception("we are testing Custome Exception")
+    except Exception as e:
+        raise HousingException(e,sys) from e
+    #     logging.info(e)
     #     logging.info("we are testing logging module")
     # return "Starting Machine Learning MODELS And Practice"
 
