128962a4b3ec27c7524b8399130e07c5538ad1ca
Amar gajula <amargajula123@gmail.com>
2025-02-23 11:58:07 +0530

    data_ingestion stage completed

---
+++ demo.py
@@ -0,0 +1,52 @@
+from housing.pipeline.pipeline import Pipeline
+from housing.logger import logging
+from housing.config.configuration import Configuration
+#from housing.component.data_transformation import DataTransformation
+
+
+def main():
+    try:
+        # +++++++++get_model_trainer_config() Check++++++++++
+        #model_traine_config = Configuration().get_model_trainer_config()
+        #print(model_traine_config)
+        # +++++++++get_model_trainer_config() Done++++++++++
+
+        # +++++++++get_data_transformation_config() Check++++++++++
+        #data_transformation_config = Configuration().get_data_transformation_config()
+        #print(data_transformation_config)
+        # +++++++++get_data_transformation_config() Done++++++++++
+
+        # +++++++++get_data_validation_config() Check++++++++++
+        #data_validation_config = Configuration().get_data_validation_config()
+        #print(data_validation_config)
+        # +++++++++get_data_validation_config() Done++++++++++
+
+        # +++++++++get_data_ingestion_config() Check++++++++++
+        #data_ingestion_config = Configuration().get_data_ingestion_config()
+        #print(data_ingestion_config)
+        # +++++++++get_data_ingestion_config() Done++++++++++
+
+
+
+        
+        # df = DataTransformation.load_data(file_path=file_path,schema_file_path=schema_file_path
+        #)
+        #print(df.columns)
+        #print(df.dtypes)
+        
+
+        # +++++++++ CHECK ANY COMPONENT ++++++++++
+
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+
+        # +++++++++CHECK ANY COMPONENT DONE++++++++++
+
+
+
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
+
+if __name__=="__main__":
+    main()
---
+++ housing/pipeline/pipeline.py
@@ -0,0 +1,101 @@
+from housing.config.configuration import Configuration
+from housing.logger import logging
+from housing.exception import HousingException
+from housing.entity.artifact_entity import DataIngestionArtifact,\
+DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact
+
+from housing.entity.config_entity import DataIngestionConfig
+from housing.component.data_ingestion import DataIngestion
+# from housing.component.data_validation import DataValidation
+# from housing.component.data_transformation import DataTransformation
+# from housing.component.model_trainer import ModelTrainer
+import os,sys
+
+
+
+
+
+class Pipeline:
+    def __init__(self,config: Configuration=Configuration()) -> None:
+        try:
+            self.config = config
+        except Exception as e:
+            raise HousingException(e,sys) from e
+        
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        try:
+            data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
+             
+            # initiate_data_ingestion() here we have download data Split data Read data we have 
+            # we used download_housing_data() funtion for downoad data
+            # we used extract_tgz_file() function for extract "housing file" data
+            # we used split_data_as_train_test() fuction for split the data int trai and test and store at
+            # ingested_train_dir and ingested_test_dir
+            return data_ingestion.initiate_data_ingestion() 
+        except Exception as e:
+            raise HousingException(e,sys) from e
+        
+    # def start_data_validation(self,
+    #                           data_ingestion_artifact:DataIngestionArtifact
+    #                           )->DataValidationArtifact:
+    #     try:
+    #         data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+    #                                          data_ingestion_artifact=data_ingestion_artifact
+    #         )
+    #         return data_validation.initiate_data_validaton()
+        
+    #     except Exception as e:
+    #         raise H(e,sys) from e
+    
+    # def start_data_transformation(self,
+    #                               data_ingestion_artifact:DataIngestionArtifact,
+    #                               data_validation_artifact:DataValidationArtifact
+    #                               )->DataTransformationArtifact:
+        
+    #     try:
+    #         data_transformation = DataTransformation(
+    #             data_transformation_config=self.config.get_data_transformation_config(),
+    #             data_ingestion_artifact= data_ingestion_artifact,
+    #             data_validation_artifact= data_validation_artifact
+    #         )
+    #         return data_transformation.initiate_data_transformation()
+    
+    #     except Exception as e:
+    #         raise HosingException(e,sys) from e
+
+    # def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact
+    #                         )->ModelTrainerArtifact:
+    #     try:
+    #         model_trainer = ModelTrainer(
+    #             model_trainer_config =self.config.get_model_trainer_config(),
+    #             data_transformation_artifact = data_transformation_artifact
+    #             )
+    #         return model_trainer.initiate_model_trainer()
+    #     except Exception as e:
+    #         raise HosingException(e,sys) from e
+
+    # def start_model_evaluation(self):
+    #     try:
+    #         pass
+    #     except Exception as e:
+    #         raise HosingException(e,sys) from e
+
+    # def start_model_pusher(self):
+    #     try:
+    #         pass
+    #     except Exception as e:
+    #         raise HosingException(e,sys) from e
+        
+    def run_pipeline(self):
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+            # data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+
+            # data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact= data_ingestion_artifact,
+            #                                                               data_validation_artifact=data_validation_artifact)
+            
+            # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            
+            
+        except Exception as e:
+            raise HousingException(e,sys) from e
